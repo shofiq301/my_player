9397690aae460e5181d60a1aaa3cd511e57d4d4c
Md Shofiulla <mdshofiulla@Mds-MacBook-Pro.local>
2022-01-17 00:19:45 +0600

    initial commit

---
+++ app/src/main/java/com/shofiq/myplayer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shofiq.myplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
